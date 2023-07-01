59e8c4f9fd27048cc10fb8cedff69a8f6f000514
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-01 05:36:50 +0100

    dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -53,33 +53,62 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
-
       </div>
-        <div className="grid gap-6 mb-8 mt-20 containter  md:grid-cols-2 xl:grid-cols-3">
-          <div className=" text-[] p-4 shadow-lg rounded-lg shadow-xs">
-            <p className="text-[35] font-bold">Productivity Bar</p>
-            <div className="flex gap-5 mt-2">
-              <BsBuildingCheck size={45} className="text-[#1843C8] mt-1" />
-              <p className="font-bold text-[35px]">25</p>
-            </div>
+      <div className="grid gap-6 mb-8 mt-20 containter  md:grid-cols-2 xl:grid-cols-3">
+        <div className=" text-[] p-4 shadow-lg rounded-lg shadow-xs">
+          <p className="text-[35] font-bold">Productivity Bar</p>
+          <div className="flex gap-5 mt-2">
+            <BsBuildingCheck size={45} className="text-[#1843C8] mt-1" />
+            <p className="font-bold text-[35px]">25</p>
           </div>
+        </div>
 
-          <div className=" text-[] p-4 shadow-lg rounded-lg shadow-xs">
-            <p className="text-[35] font-bold">Top Project Progress</p>
-            <div className="flex gap-5 mt-2">
-              <BsBuildingCheck size={45} className="text-[#1843C8] mt-1" />
-              <p className="font-bold text-[35px]">25</p>
+        <div className=" text-[] p-5 shadow-lg rounded-lg shadow-xs">
+          <p className="text-[35] font-bold">Top Project Progress</p>
+          <div className="flex gap-4 mt-2">
+            <div className="p-2">
+              <PieChart
+                data={[
+                  { title: "One", value: 10, color: "#1843C8" },
+                  { title: "Two", value: 55, color: "#E5EFFC" },
+                ]}
+              />
+
+              <p className="font-bold">Casvita</p>
             </div>
-          </div>
-
-          <div className=" text-[] p-4 shadow-lg rounded-lg shadow-xs">
-            <p className="text-[35] font-bold">Daily Work Progress</p>
-            <div className="flex gap-5 mt-2">
-              <BsBuildingCheck size={45} className="text-[#1843C8] mt-1" />
-              <p className="font-bold text-[35px]">25</p>
+            <div className="p-2">
+              <PieChart
+                data={[
+                  { title: "One", value: 17, color: "#1843C8" },
+                  { title: "Two", value: 15, color: "#E5EFFC" },
+                ]}
+              />
+              <p className="font-bold">Bohikor</p>
+            </div>
+            <div className="p-2">
+              <PieChart
+                data={[
+                  { title: "One", value: 10, color: "#1843C8" },
+                  { title: "Two", value: 15, color: "#E5EFFC" },
+                ]}
+              />
+              <p className="font-bold">Cliqets</p>
             </div>
           </div>
         </div>
+
+        <div className=" text-[] p-4 shadow-lg rounded-lg shadow-xs">
+          <p className="text-[35] font-bold">Daily Work Progress</p>
+          <div className="flex gap-5 mt-2">
+            <BsBuildingCheck size={45} className="text-[#1843C8] mt-1" />
+            <p className="font-bold text-[35px]">25</p>
+          </div>
+        </div>
+      </div>
+      <div className="grid gap-6 mb-8 mt-20 containter shadow-xl">
+
+      </div>
+
     </div>
   );
 }
