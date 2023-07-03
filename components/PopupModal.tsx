930ef34e62d144f88a54807b012533aadd81f2fc
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-03 08:48:16 +0100

    delete comment

--- components/PopupModal.tsx
+++ components/PopupModal.tsx
@@ -1,37 +1,6 @@
 import { useRouter } from 'next/router';
 import React from 'react'
 
-// export default function PopupModal() {
-//   return (
-//     <div>
-
-// <button id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown header <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-
-
-// <div id="dropdownInformation" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-//     <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-//       <div>Bonnie Green</div>
-//       <div className="font-medium truncate">name@flowbite.com</div>
-//     </div>
-//     <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
-//       <li>
-//         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-//       </li>
-//       <li>
-//         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-//       </li>
-//       <li>
-//         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-//       </li>
-//     </ul>
-//     <div className="py-2">
-//       <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-//     </div>
-// </div>
-
-//     </div>
-//   )
-// }
 
 type props = {
     setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
